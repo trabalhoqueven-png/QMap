ced4e278b149ac8cc494e046031d957212c21a10
Queven <trabalhoqueven@gmail.com>
2026-02-05 12:41:51 -0300

    Update Mapa.js
    Signed-off-by: Queven <trabalhoqueven@gmail.com>

--- Mapa.js
+++ Mapa.js
@@ -1,33 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import {
-  getFirestore,
-  doc,
-  getDoc
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import {
-  getAuth,
-  onAuthStateChanged
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+const map = L.map("map").setView([-15.78, -47.93], 5);
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAT37Au3gOEqT2KzG9eaxE41X_U4mZez4o",
-  authDomain: "qmap-981c5.firebaseapp.com",
-  projectId: "qmap-981c5"
-};
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© OpenStreetMap"
+}).addTo(map);
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-updateDoc(doc(db, "users", uid), {
-  credits: increment(qtd)
+// Clique para criar marcação
+map.on("click", function (e) {
+  const marker = L.marker(e.latlng).addTo(map);
+  marker.bindPopup("🎰 Ponto do Cassino").openPopup();
 });
-
-onAuthStateChanged(auth, async user => {
-  if (!user) return location.href = "index.html";
-
-  const snap = await getDoc(doc(db, "users", user.uid));
-  document.getElementById("saldo").innerText =
-    snap.data().credits + " créditos";
-});
-
